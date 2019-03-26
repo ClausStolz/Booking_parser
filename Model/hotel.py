ca54cbda3ff7939d1796b45e2cae6d9f71fbac45
Николай Глущенко <clausstolz@yandex.ru>
2019-03-26 20:37:12 +1000

    Rename variable

--- Model/hotel.py
+++ Model/hotel.py
@@ -1,9 +1,9 @@
 
 class Hotel:
 
-    def __init__(self, aName, aAdress, aDescription, aServices, aApartment):
+    def __init__(self, aName, aAdress, aDescription, aServices, aApartments):
         self.Name = aName
         self.Adress = aAdress
         self.Description = aDescription
         self.Services = aServices
-        self.Apartment = aApartment
+        self.Apartments = aApartments
