da0816d1c09d328c300e494063e3be1301a25139
Николай Глущенко <clausstolz@yandex.ru>
2019-03-27 13:42:52 +1000

    Fix saving json file

--- index.py
+++ index.py
@@ -9,14 +9,17 @@ parseController = cntr.ParserController()
 
 url = "https://www.booking.com/searchresults.ru.html?ss=Vladivostok"
 offset = 0
-while (offset <= 500):
-    hotel_list = parseController.gen_hotel_list(url + "&rows=50&offset=" + str(offset))
-    offset += 50
+with io.open('hotels.json', 'w', encoding='utf8') as json_file:
+    json_file.write("[")
+    while (offset <= 500):
+        hotel_list = parseController.gen_hotel_list(url + "&rows=15&offset=" + str(offset))
+        offset += 15
 
-    with io.open('hotels.json', 'w', encoding='utf8') as json_file:
-        json_file.write("[")
         for i in hotel_list:
-            h = parseController.gen_hotel("https://www.booking.com" + i)
-            json_file.write(h.toJSON())
-            json_file.write(",")
-        json_file.write("]")
+            try:
+                h = parseController.gen_hotel("https://www.booking.com" + i)
+                json_file.write(h.toJSON())
+                json_file.write(",")
+            except:
+                pass
+    json_file.write("]")
