bd7c487d785d5b73f7caab2afd841445dc5f57d2
Николай Глущенко <clausstolz@yandex.ru>
2019-03-27 13:51:37 +1000

    Refactor aparment model

--- Model/apartment.py
+++ Model/apartment.py
@@ -1,7 +1,7 @@
 
 class Apartment:
 
-    def __init__(self, aName, aCapacity, aBed_types):
-        self.Name = aName
-        self.Capacity = aCapacity
-        self.Bed_types = aBed_types
+    def __init__(self, a_name, a_capacity, a_bed_types):
+        self.name = a_name
+        self.capacity = a_capacity
+        self.bed_types = a_bed_types
